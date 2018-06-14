35a7d0dbd01f046bfdb2f1faf9be6bd2bfd9f7bc
Amber Elferink <a.s.elferink@students.uu.nl>
2018-06-15 00:08:45 +0200

    Phong shading werkt ongeveer volgens mij

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -20,8 +20,19 @@ void main()
 	float dist = L.length();
 	L = normalize( L );
 	vec3 lightColor = vec3( 10, 10, 8 );
+	vec3 specLightColor = vec3( 2, 2, 2);
+
+	vec3 ambientColor = vec3(1, 0.1f, 0.1f );
 	vec3 materialColor = texture( pixels, uv).xyz;
+	vec3 diffuseColor = materialColor;
+	vec3 specularColor = vec3(1, 1, 1);
+
 	float attenuation = 1.0f / (dist* dist);
-	outputColor = vec4( materialColor* max( 0.0f, dot( L, normal.xyz) ) * 
-		attenuation * lightColor, 1 );
+
+	float alfa = 0.3f;
+
+	vec3 Rv = L;
+
+	outputColor = vec4( ambientColor + diffuseColor * max( 0.0f, dot( L, normal.xyz) ) * attenuation * lightColor + 
+		specularColor * pow(max( 0.0f, dot( L, Rv) ), alfa) * attenuation * specLightColor, 1 );
 }
