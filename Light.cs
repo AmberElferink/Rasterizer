8921bd10658a9c31fb6863808c2ff08e00e508d7
Miriam <m.f.sterl@uu.nl>
2018-06-18 16:00:25 +0200

    4 lichten als uniform variables gestuurd
    in game vier lichten aangemaakt; in fs deze gebruikt om de totale kleur van een punt te berekenen.

---
+++ Light.cs
@@ -0,0 +1,32 @@
+﻿using OpenTK;
+using System;
+
+public class Light
+{
+    public Vector3 position; // position of the light in world space
+    public Vector3 color; // light color
+    public Vector3 specular; // specular light color
+
+    public Light(Vector3 pos, Vector3 col, Vector3 spec)
+    {
+        position = pos;
+        color = col;
+        specular = spec;
+    }
+
+    public Vector3 lightPos
+    {
+        get { return position; }
+    }
+
+    public Vector3 lightColor
+    {
+        get { return color; }
+    }
+
+    public Vector3 specLightColor
+    {
+        get { return specular; }
+    }
+}
+
--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -8,26 +8,43 @@ in vec4 worldPos;           // world space position of fragment
 out vec4 outputColor;       
 
 // texture sampler
-uniform sampler2D pixels;   
-
-// light position in world space
-uniform vec3 lightPos;    
+uniform sampler2D pixels; 
 
 // ambient light color
 uniform vec3 ambientColor;
 
+// lights stored in matrices (position, color, specular color)
+uniform mat3 light1;
+uniform mat3 light2;
+uniform mat3 light3;
+uniform mat3 light4;
+uniform int lightCount;
+// TODO: kan ik ook direct de lijst met lichten doorgeven? (Of array?)
+// Nu wordt wel het aantal lichten doorgegeven (lightCount), maar omdat we toch alle lichten apart moeten declareren hebben we daar niks aan.
+
 // fragment shader
 void main()             
 {
+	outputColor = vec4(ambientColor, 1); // mag maar één keer worden opgeteld (niet in elke loop opnieuw)
+	
+	vec3 vecx = vec3(1,0,0);
+	vec3 vecy = vec3(0,1,0);
+	vec3 vecz = vec3(0,0,1);
+
+	mat3 lights[4] = mat3[4](light1, light2, light3, light4);
+	// TODO: hoe kun je eerst een array (of list?) declareren en daarna pas elementen toevoegen?
+	// Nu hebben we namelijk nog altijd niks aan het weten van lightCount, omdat we alsnog alles 'met de hand' moeten toevoegen.
+
+	for (int i = 0; i < 4; i++)
+	{
+	vec3 lightPos = lights[i]*vecx;
+	vec3 lightColor = lights[i]*vecy;
+	vec3 specLightColor = lights[i]*vecz;
+	
 	vec3 L = lightPos - worldPos.xyz;
 	float dist = L.length();
-	L = normalize( L );
-	vec3 lightColor = vec3( 10, 10, 8 );
-	//vec3 specLightColor = vec3( 2, 2, 2);
-	vec3 specLightColor = vec3( 5, 5, 5);
+	L = normalize( L );	
 
-	//vec3 ambientColor = vec3(1, 0.1f, 0.1f );
-	//vec3 ambientColor = vec3(0.5f, 0.05f, 0.05f );
 	vec3 materialColor = texture( pixels, uv).xyz;
 	vec3 diffuseColor = materialColor;
 	vec3 specularColor = vec3(1, 1, 1);
@@ -39,10 +56,11 @@ void main()
 	vec3 Rv = -L + 2*dot(L,normal.xyz)*normal.xyz;
 	Rv = normalize(Rv);
 
-	outputColor = vec4( ambientColor + diffuseColor * max( 0.0f, dot( L, normal.xyz) ) * attenuation * lightColor + 
+	outputColor += vec4(diffuseColor * max( 0.0f, dot( L, normal.xyz) ) * attenuation * lightColor + 
 		specularColor * pow(max( 0.0f, dot( L, Rv) ), alfa) * attenuation * specLightColor, 1 );
+	}
+	
 
 		// TODO: is het idee niet dat de felle vlek kleiner wordt naarmate alfa groter wordt? Hier lijkt de vlek gewoon te verdwijnen.
 		// TODO: waarom zitten er meerdere lichte plekken op de theepot, terwijl er maar één lichtbron is? (Er is altijd een vlek aan de linkerkant.)
-		// TODO: what is a 'uniform' variable?
 }
